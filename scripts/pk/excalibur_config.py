611ad1c3e03277777af2f18153c5535dbda992b9
Christophe <cbramdit@gmail.com>
2024-10-28 14:56:06 +0100

    Fixed a bug in the GOAT config file

--- scripts/pk/excalibur_config.py
+++ scripts/pk/excalibur_config.py
@@ -40,8 +40,8 @@ class ExcaliburConfig(StrategyV2ConfigBase):
     position_mode: PositionMode = PositionMode.ONEWAY
 
     # Triple Barrier
-    stop_loss_pct: Decimal = Field(1.75, client_data=ClientFieldData(is_updatable=True))
-    trailing_stop_activation_pct: Decimal = Field(2, client_data=ClientFieldData(is_updatable=True))
+    stop_loss_pct: Decimal = Field(1.5, client_data=ClientFieldData(is_updatable=True))
+    trailing_stop_activation_pct: Decimal = Field(1.6, client_data=ClientFieldData(is_updatable=True))
     trailing_stop_close_delta_pct: Decimal = Field(1.5, client_data=ClientFieldData(is_updatable=True))
 
     # Technical analysis
