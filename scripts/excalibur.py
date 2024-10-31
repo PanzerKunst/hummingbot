3b535decdef19223d803223a3585eedcf8b12ba4
Christophe <cbramdit@gmail.com>
2024-10-31 13:06:40 +0100

    Bugfix: Too many orders were opened when SMA crossing

--- scripts/excalibur.py
+++ scripts/excalibur.py
@@ -120,13 +120,13 @@ class ExcaliburStrategy(PkStrategy):
         filled_sell_orders, filled_buy_orders = self.get_filled_tracked_orders_by_side()
 
         if len(filled_sell_orders) > 0:
-            if self.is_latest_short_sma_over_long():
-                self.logger().info("stop_actions_proposal() > Short SMA is over long")
+            if self.did_short_sma_cross_over_long():
+                self.logger().info("stop_actions_proposal() > Short SMA crossed over long")
                 self.market_close_orders(filled_sell_orders, CloseType.COMPLETED)
 
         if len(filled_buy_orders) > 0:
-            if not self.is_latest_short_sma_over_long():
-                self.logger().info("stop_actions_proposal() > Short SMA is under long")
+            if self.did_short_sma_cross_under_long():
+                self.logger().info("stop_actions_proposal() > Short SMA crossed under long")
                 self.market_close_orders(filled_buy_orders, CloseType.COMPLETED)
 
         return []  # Always return []
@@ -178,10 +178,10 @@ class ExcaliburStrategy(PkStrategy):
     #
 
     def get_latest_sma(self, short_or_long: str) -> float:
-        return self._get_sma_at_index(short_or_long, -1)
+        return self._get_sma_at_index(short_or_long, -2)
 
     def get_previous_sma(self, short_or_long: str) -> float:
-        return self._get_sma_at_index(short_or_long, -2)
+        return self._get_sma_at_index(short_or_long, -3)
 
     def _get_sma_at_index(self, short_or_long: str, index: int) -> float:
         return self.processed_data[f"SMA_{short_or_long}"].iloc[index]
