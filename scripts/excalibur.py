e77e6e9f50897ce93677aa97c3846a3d04b3451c
Christophe <cbramdit@gmail.com>
2024-11-10 20:00:25 +0100

    Reduced cooldown on MR trades

--- scripts/excalibur.py
+++ scripts/excalibur.py
@@ -204,7 +204,7 @@ class ExcaliburStrategy(PkStrategy):
             asyncio.get_running_loop().create_task(self.create_twap_market_orders(TradeType.BUY, entry_price, triple_barrier, ORDER_REF_MEAN_REVERSION))
 
     def can_create_mean_reversion_order(self, side: TradeType, active_tracked_orders: List[TrackedOrderDetails]) -> bool:
-        if not self.can_create_order(side, ORDER_REF_MEAN_REVERSION, 10):
+        if not self.can_create_order(side, ORDER_REF_MEAN_REVERSION, 3):
             return False
 
         if len(active_tracked_orders) > 0:
