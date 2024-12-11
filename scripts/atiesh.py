6c155f6f0d57f1c7b98a0c6518debf0ce1cb55cd
Christophe <christophe@8b.nu>
2024-12-11 17:36:46 +0100

    Increased stoch_reversal_counter to 10 for closing

--- scripts/atiesh.py
+++ scripts/atiesh.py
@@ -508,7 +508,7 @@ class ExcaliburStrategy(PkStrategy):
         self.rsi_reversal_counter += 1
         self.logger().info(f"has_rsi_bottomed() | incremented self.rsi_reversal_counter to:{self.rsi_reversal_counter}")
 
-        if self.rsi_reversal_counter < 3:
+        if self.rsi_reversal_counter < 5:
             return False
 
         too_late_threshold: Decimal = rsi_threshold + rsi_increment
@@ -577,7 +577,7 @@ class ExcaliburStrategy(PkStrategy):
         self.stoch_reversal_counter += 1
         self.logger().info(f"should_close_rev_sell_due_to_stoch_reversal() | incremented self.stoch_reversal_counter to:{self.stoch_reversal_counter}")
 
-        return self.stoch_reversal_counter > 2
+        return self.stoch_reversal_counter > 9
 
     def should_close_rev_buy_due_to_stoch_reversal(self, filled_buy_orders: List[TrackedOrderDetails]) -> bool:
         # Don't close if we just opened
@@ -619,4 +619,4 @@ class ExcaliburStrategy(PkStrategy):
         self.stoch_reversal_counter += 1
         self.logger().info(f"should_close_rev_buy_due_to_stoch_reversal() | incremented self.stoch_reversal_counter to:{self.stoch_reversal_counter}")
 
-        return self.stoch_reversal_counter > 2
+        return self.stoch_reversal_counter > 9
