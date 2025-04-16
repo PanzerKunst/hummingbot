7e9af0d0b0e42d19234d9e2bb5119015217d9242
Christophe <christophe@8b.nu>
2025-04-16 13:20:42 +0200

    WIP

--- hummingbot/connector/derivative/hyperliquid_perpetual/hyperliquid_perpetual_derivative.py
+++ hummingbot/connector/derivative/hyperliquid_perpetual/hyperliquid_perpetual_derivative.py
@@ -28,7 +28,7 @@ from hummingbot.core.data_type.in_flight_order import InFlightOrder, OrderUpdate
 from hummingbot.core.data_type.order_book_tracker_data_source import OrderBookTrackerDataSource
 from hummingbot.core.data_type.trade_fee import TokenAmount, TradeFeeBase
 from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
-from hummingbot.core.utils.async_utils import safe_ensure_future
+from hummingbot.core.utils.async_utils import safe_ensure_future, safe_gather
 from hummingbot.core.utils.estimate_fee import build_trade_fee
 from hummingbot.core.web_assistant.web_assistants_factory import WebAssistantsFactory
 
@@ -211,13 +211,12 @@ class HyperliquidPerpetualDerivative(PerpetualDerivativePyBase):
         )
 
     async def _status_polling_loop_fetch_updates(self):
-        pass
-        # await safe_gather(
-        #             self._update_trade_history(),
-        #             self._update_order_status(),
-        #             self._update_balances(),
-        #             self._update_positions(),
-        # )
+        await safe_gather(
+            self._update_trade_history(),
+            self._update_order_status(),
+            self._update_balances(),
+            self._update_positions(),
+        )
 
     async def _update_order_status(self):
         await self._update_orders()
@@ -388,7 +387,7 @@ class HyperliquidPerpetualDerivative(PerpetualDerivativePyBase):
                 "isBuy": True if trade_type is TradeType.BUY else False,
                 "limitPx": float(price),
                 "sz": float(amount),
-                "reduceOnly": False,
+                "reduceOnly": position_action == PositionAction.CLOSE,
                 "orderType": param_order_type,
                 "cloid": order_id,
             }
@@ -482,20 +481,29 @@ class HyperliquidPerpetualDerivative(PerpetualDerivativePyBase):
 
     async def _request_order_status(self, tracked_order: InFlightOrder) -> OrderUpdate:
         client_order_id = tracked_order.client_order_id
+        try:
+            if tracked_order.exchange_order_id:
+                exchange_order_id = tracked_order.exchange_order_id
+            else:
+                exchange_order_id = await tracked_order.get_exchange_order_id()
+        except asyncio.TimeoutError:
+            exchange_order_id = None
         order_update = await self._api_post(
             path_url=CONSTANTS.ORDER_URL,
             data={
                 "type": CONSTANTS.ORDER_STATUS_TYPE,
                 "user": self.hyperliquid_perpetual_api_key,
-                "oid": int(tracked_order.exchange_order_id) if tracked_order.exchange_order_id else client_order_id
+                "oid": int(exchange_order_id) if exchange_order_id else client_order_id
             })
         current_state = order_update["order"]["status"]
+        _exchange_order_id = str(tracked_order.exchange_order_id) if tracked_order.exchange_order_id else str(
+            order_update["order"]["order"]["oid"])
         _order_update: OrderUpdate = OrderUpdate(
             trading_pair=tracked_order.trading_pair,
             update_timestamp=order_update["order"]["order"]["timestamp"] * 1e-3,
             new_state=CONSTANTS.ORDER_STATE[current_state],
             client_order_id=order_update["order"]["order"]["cloid"] or client_order_id,
-            exchange_order_id=str(tracked_order.exchange_order_id),
+            exchange_order_id=_exchange_order_id,
         )
         return _order_update
 
@@ -604,6 +612,7 @@ class HyperliquidPerpetualDerivative(PerpetualDerivativePyBase):
             self.logger().debug(f"Ignoring order message with id {client_order_id}: not in in_flight_orders.")
             return
         current_state = order_msg["status"]
+        tracked_order.update_exchange_order_id(str(order_msg["order"]["oid"]))
         order_update: OrderUpdate = OrderUpdate(
             trading_pair=tracked_order.trading_pair,
             update_timestamp=order_msg["statusTimestamp"] * 1e-3,
@@ -636,12 +645,14 @@ class HyperliquidPerpetualDerivative(PerpetualDerivativePyBase):
                 step_size = Decimal(str(10 ** -coin_info.get("szDecimals")))
 
                 price_size = Decimal(str(10 ** -len(price_info.get("markPx").split('.')[1])))
+                _min_order_size = Decimal(str(10 ** -len(price_info.get("openInterest").split('.')[1])))
                 collateral_token = CONSTANTS.CURRENCY
                 return_val.append(
                     TradingRule(
                         trading_pair,
                         min_base_amount_increment=step_size,
                         min_price_increment=price_size,
+                        min_order_size=_min_order_size,
                         buy_order_collateral_token=collateral_token,
                         sell_order_collateral_token=collateral_token,
                     )
---
+++ scripts/okx_perp_missing_2s.py
@@ -0,0 +1,136 @@
+import os
+from decimal import Decimal
+from typing import Dict, List
+
+import pandas as pd
+import pandas_ta as ta  # noqa: F401
+from pydantic import Field
+
+from hummingbot.client.ui.interface_utils import format_df_for_printout
+from hummingbot.connector.connector_base import ConnectorBase
+from hummingbot.core.clock import Clock
+from hummingbot.core.data_type.common import PositionMode
+from hummingbot.data_feed.candles_feed.candles_factory import CandlesConfig
+from hummingbot.strategy.strategy_v2_base import StrategyV2Base, StrategyV2ConfigBase
+from hummingbot.strategy_v2.models.executor_actions import CreateExecutorAction, StopExecutorAction
+
+# Generate config file: create --script-config okx_perp_missing_2s
+# Start the bot: start --script okx_perp_missing_2s.py --conf conf_okx_perp_missing_2s_FARTCOIN_Hyperliquid.yml
+#                start --script okx_perp_missing_2s.py --conf conf_okx_perp_missing_2s_FARTCOIN_OKX.yml
+
+SHORT_MA_LENGTH: int = 28
+
+
+class OkxMissingTwoSecConfig(StrategyV2ConfigBase):
+    script_file_name: str = Field(default_factory=lambda: os.path.basename(__file__))
+    markets: Dict[str, List[str]] = {}
+    candles_config: List[CandlesConfig] = []
+    controllers_config: List[str] = []
+    exchange: str = "hyperliquid_perpetual"
+    trading_pair: str = "FARTCOIN-USD"
+    candles_exchange: str = "binance_perpetual"
+    candles_pair: str = "FARTCOIN-USDT"
+    candles_interval: str = "1m"
+    candles_length: int = 60
+    order_amount_quote: Decimal = 30
+    leverage: int = 3
+    position_mode: PositionMode = PositionMode.ONEWAY
+
+
+class OkxMissingTwoSec(StrategyV2Base):
+    account_config_set = False
+
+    @classmethod
+    def init_markets(cls, config: OkxMissingTwoSecConfig):
+        cls.markets = {config.exchange: {config.trading_pair}}
+
+    def __init__(self, connectors: Dict[str, ConnectorBase], config: OkxMissingTwoSecConfig):
+        if len(config.candles_config) == 0:
+            config.candles_config.append(CandlesConfig(
+                connector=config.candles_exchange,
+                trading_pair=config.candles_pair,
+                interval=config.candles_interval,
+                max_records=config.candles_length + 10
+            ))
+        super().__init__(connectors, config)
+        self.config = config
+
+    def start(self, clock: Clock, timestamp: float) -> None:
+        """
+        Start the strategy.
+        :param clock: Clock to use.
+        :param timestamp: Current time.
+        """
+        self._last_timestamp = timestamp
+        self.apply_initial_setting()
+
+    def apply_initial_setting(self):
+        if not self.account_config_set:
+            for connector_name, connector in self.connectors.items():
+                if self.is_perpetual(connector_name):
+                    connector.set_position_mode(self.config.position_mode)
+                    for trading_pair in self.market_data_provider.get_trading_pairs(connector_name):
+                        connector.set_leverage(trading_pair, self.config.leverage)
+            self.account_config_set = True
+
+    def update_processed_data(self):
+        candles_config = self.config.candles_config[0]
+
+        candles_df = self.market_data_provider.get_candles_df(connector_name=candles_config.connector,
+                                                              trading_pair=candles_config.trading_pair,
+                                                              interval=candles_config.interval,
+                                                              max_records=candles_config.max_records)
+        num_rows = candles_df.shape[0]
+
+        if num_rows == 0:
+            return
+
+        candles_df["index"] = candles_df["timestamp"]
+        candles_df.set_index("index", inplace=True)
+
+        candles_df["timestamp_iso"] = pd.to_datetime(candles_df["timestamp"], unit="s")
+
+        candles_df[f"EMA_{SHORT_MA_LENGTH}"] = candles_df.ta.ema(length=SHORT_MA_LENGTH)
+
+        candles_df.dropna(inplace=True)
+
+        self.processed_data = candles_df
+
+    def create_actions_proposal(self) -> List[CreateExecutorAction]:
+        self.update_processed_data()
+
+        self.logger().info(f"create_actions_proposal() | close:{self.get_current_close()}")
+
+        create_actions = []
+        return create_actions
+
+    def stop_actions_proposal(self) -> List[StopExecutorAction]:
+        stop_actions = []
+        return stop_actions
+
+    def format_status(self) -> str:
+        original_status = super().format_status()
+        custom_status = ["\n"]
+
+        if self.ready_to_trade:
+            if not self.processed_data.empty:
+                columns_to_display = [
+                    "timestamp_iso",
+                    "low",
+                    "high",
+                    "close",
+                    "volume",
+                    f"EMA_{SHORT_MA_LENGTH}"
+                ]
+
+                custom_status.append(format_df_for_printout(self.processed_data[columns_to_display].tail(20), table_format="psql"))
+
+        return original_status + "\n".join(custom_status)
+
+    #
+    # Getters on `self.processed_data[]`
+    #
+
+    def get_current_close(self) -> Decimal:
+        close_series: pd.Series = self.processed_data["close"]
+        return Decimal(close_series.iloc[-1])
